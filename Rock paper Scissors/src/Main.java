24504329b4d92545aea92af25a5e06c5db0fd726
Lioune Porte <1169356@learn.vsb.bc.ca>
2021-02-17 12:14:25 -0800

    x

--- Rock paper Scissors/src/Main.java
+++ Rock paper Scissors/src/Main.java
@@ -1,17 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int random, rock, paper, scissors;
+        int random, rock, paper, scissors, number;
         rock = 0;
         paper = 1;
         scissors = 2;
         //System.out.println((int)(Math.random()*3)); //results less than 3
-        if (0 == rock)
+        /* if (0 == rock)
             System.out.println("rock");
         else
         if (paper == 1)
             System.out.println("paper");
         else
         if (scissors == 2)
-            System.out.println("scissors");
+            System.out.println("scissors");*/
+        System.out.println((int) (Math.random()*3)+1);
+
     }
 }
