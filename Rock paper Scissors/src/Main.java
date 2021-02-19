9c0dd758fbd208936c7c354fe5980efbfaf10ff7
Kazually <78816873+Kazually@users.noreply.github.com>
2021-02-19 12:50:26 -0800

    m

--- Rock paper Scissors/src/Main.java
+++ Rock paper Scissors/src/Main.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int random;
-        Scanner scanner = new Scanner (System.in);
         System.out.println("Rock = 1, Paper = 2, Scissors = 3, Spock = 4, 5 = shrug");
-        System.out.println(" ");
+        /*System.out.println(" ");
         System.out.println("| 1 |               " + "Rock  > Scissors" + " | Rock  < Paper" + " | Rock  > Spock");
         System.out.println(" ");
         System.out.println("| 2 |               " + "Paper > Rock" + " | Paper < Scissors" + " | Paper > Spock");
         System.out.println(" ");
-        System.out.println("| 3 |               " + "Scissors > Paper" + " | Scissors < Rock" + " | Scissors < Spock");
+        System.out.println("| 3 |               " + "Scissors > Paper" + " | Scissors < Rock" + " | Scissors < Spock"); */
         System.out.println(" ");
         System.out.println("| 4 |               " + "Spock  > Rock" + " | Spock  < Paper" + " | Spock  > Scissors");
         System.out.println(" ");
+        Scanner scanner = new Scanner (System.in);
         String name = scanner.next();
         random = (int)(Math.random() * 5 + 1);
         System.out.println(random);
