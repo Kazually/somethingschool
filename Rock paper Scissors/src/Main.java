dfd1d2fdf86ef127b08a6ef086b271909929be39
Lioune Porte <1169356@learn.vsb.bc.ca>
2021-02-22 12:31:18 -0800

    Final

--- Rock paper Scissors/src/Main.java
+++ Rock paper Scissors/src/Main.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Spock > Rock, Spock < Paper, Spock > Scissors");
+        System.out.println("Spock > Rock, Spock < Paper, Spock > Scissors. " +
+                "Type end to end game");
         System.out.println("""
                 |------------|
                 |1 - Rock    |
@@ -11,90 +12,81 @@ public class Main {
                 |3 - Paper   |
                 |4 - Spock   | 
                 |------------|""");
-        int random;
-        int Pwin, Cwin, Player, Computer;
-        Pwin = 0;
-        Cwin = 0;
-        Player = 0;
-        Computer = 0;
-        while (Math.abs(Cwin - Pwin) < 2){
-            Scanner input = new Scanner (System.in);
-            Player = input.nextByte();
-            Computer = (int)(Math.random() * 5 + 1);
-            if (Player == Computer)
-                if(Computer == 1){
-                    System.out.println("tie");
-                }
-                else if (Computer == 2){
-                    System.out.println("tie");
-                }
-                else if(Computer == 3){
-                    System.out.println("tie");
-                }
-                else{
-                    System.out.println("tie");
-                }
-                //player = 1
-            else if(Player == 1){
-                if (Computer == 2){
-                    System.out.println("win");
-                    Pwin ++;
-                }
-                else if (Computer == 3){
-                    System.out.println("lose");
-                    Cwin ++;
-                }
-                else{
-                    System.out.println("Lose");
-                    Cwin ++;
-                }
-            }
-            //player = 2
-            else if(Player == 2){
-                if (Computer == 3){
-                    System.out.println("Win");
-                    Pwin ++;
-                }
-                else if (Computer == 4){
-                    System.out.println("lose");
-                    Cwin ++;
-                }
-                else{
-                    System.out.println("win");
-                    Pwin ++;
-                }
-            }
-            //player = 3
-            else if(Player == 3){
-                if (Computer == 4){
-                    System.out.println("win");
-                    Pwin ++;
-                }
-                else if (Computer == 2){
-                    System.out.println("lose");
-                    Cwin ++;
-                }
-                else{
-                    System.out.println("win");
-                    Pwin ++;
-                }
-            }
-            //player = 4
-            else if(Player == 4){
-                if (Computer == 1){
-                    System.out.println("lose");
-                    Cwin ++;
-                }
-                else if (Computer == 2){
-                    System.out.println("lose");
-                    Cwin ++;
-                }
-                else{
-                    System.out.println("Win");
-                    Cwin ++;
-                }
-            }
+        boolean RPSSe = true; //code keep going unless typed 'end'
+        Scanner scan = new Scanner(System.in); //scans for user input
+        while (RPSSe) {
+            String Player = scan.next(); //looks for next input from player
 
+            //player = rock
+            if (Player.equals("1")) {
+                //player input, then computer picks number,
+                // checks if its a lost or tie
+                int Computer = (int) (Math.random() * 4 + 1);
+                System.out.println(Computer);
+
+                if (Computer == 1)
+                    System.out.println("Tie");
+                if (Computer == 2)
+                    System.out.println("You Win");
+                else if (Computer == 3)
+                    System.out.println("You Lose");
+                else if (Computer == 4)
+                        System.out.println("You Lose");
+            }
+            //player = scissors
+            else if (Player.equals("2")) {
+
+                int Computer = (int) (Math.random() * 4 + 1);
+                System.out.println(Computer);
+
+                if (Computer == 2)
+                System.out.println("Tie");
+                if (Computer == 1)
+                    System.out.println("You Lose");
+                else if (Computer == 3)
+                    System.out.println("You Win");
+                else if (Computer == 4)
+                        System.out.println("You Lose");
+            }
+            //player = paper
+            else if (Player.equals("3")) {
+
+                int Computer = (int) (Math.random() * 4 + 1);
+                System.out.println(Computer);
+
+                if (Computer == 3)
+                System.out.println("Tie");
+                if (Computer == 2)
+                    System.out.println("You Lose");
+                else if (Computer == 1)
+                    System.out.println("You Win");
+                else if (Computer == 4)
+                        System.out.println("You Win");
+            }
+            //player = Spock
+            else if (Player.equals("4")) {
+
+                int Computer = (int) (Math.random() * 4 + 1);
+                System.out.println(Computer);
+
+                if (Computer == 4)
+                System.out.println("Tie");
+                if (Computer == 2)
+                    System.out.println("You Win");
+                else if (Computer == 3)
+                    System.out.println("You Lose");
+                else if (Computer == 1)
+                        System.out.println("You Win");
+            }
+            //if player doesnt want to play anymore, types end, then the while loop finishes
+            else if (Player.equals("end")) {
+                System.out.println("You ended game.");
+                RPSSe = false;
+            }
+            //if player doesnt put 1-4 it says invalid
+            else{
+                System.out.println("Invalid. Rock = 1, Scissors = 2, Paper = 3, Spock = 4");
+            }
         }
     }
 }
